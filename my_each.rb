792473584945a04fa85fce93c9d2eb2da2c690ee
Sarah Jacob <smjacob91@gmail.com>
2018-07-06 09:57:06 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,15 +1,8 @@
 def my_each(array)
-  
-  
-end
-
-
-def my_each(array) # put argument(s) here
-i=0
-while i<array.length 
-yield(array[i])
-i+=1 
-end
-array
-  # code here
+  i = 0
+  while i < array.length
+    yield array[i]
+    i= i + 1
+  end
+  array
 end
